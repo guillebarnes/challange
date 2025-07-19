9042c822df66dfc9e1c2a5419ba9dc3760600f9b
guillebarnes <guillebarnes_95@hotmail.com>
2025-07-19 10:24:16 -0300

    cambio en querys

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into usuario (user,pass) values ('gino','1234');
 insert into cliente (nombre, apellido,user_id) values ('GUILLERMO','BARNES',1);
 insert into estado_carrito (descripcion) values ('ABIERTO');
-insert into estado_carrito (descripcion) values ('FINALIZADO');
+insert into estado_carrito (descripcion) values ('PROCESADO');
 insert into categoria(DESCRIPCION ,DESCUENTO ) values ('CELULARES',10);
 INSERT INTO CATEGORIA (DESCRIPCION ,DESCUENTO ) values ('VARIOS',5);
 INSERT INTO PRODUCTO (DESCRIPCION ,EN_PROMOCION ,PRECIO ,CATEGORIA_ID ) VALUES ('IPHONE 15', false, 1200, 1);
