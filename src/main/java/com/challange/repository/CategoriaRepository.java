f9778a22a1f200e98d2b22a1f33eaed4f2e18238
guillebarnes <guillebarnes_95@hotmail.com>
2025-07-19 20:48:07 -0300

    dao de categoria

---
+++ src/main/java/com/challange/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.challange.repository;
+
+import com.challange.entity.CategoriaEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<CategoriaEntity, Long> {
+}
